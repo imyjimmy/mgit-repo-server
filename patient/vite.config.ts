07a047c9b2c5badf205f25b7a9e81592dc5a8189
imyjimmy <imyjimmy@gmail.com>
2025-08-29 20:12:30 -0500

    trying out shadcn themes

--- patient/vite.config.ts
+++ patient/vite.config.ts
@@ -1,8 +1,10 @@
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/patient/',
   build: {
     outDir: 'dist',
@@ -12,6 +14,11 @@ export default defineConfig({
         manualChunks: undefined
       }
     }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     port: 5174,
